76098ac5d519e5467603609c847c94708a2a289d
João Dantas <joaodantastx1@gmail.com>
2025-01-14 19:05:17 -0300

    cass component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+ plano = {
+  infos: {
+    tipo: 'Simples',
+    preco: 100,
+  }
+ };
 }
